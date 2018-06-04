3414995d4f5351814c6624dd8ccf97106f492119
huluwa <422123725@qq.com>
2018-06-04 17:04:50 +0800

    暂存

--- api/dbInit.js
+++ api/dbInit.js
@@ -5,7 +5,7 @@ var dbInit = function () {
   sql.connect(function (err) {
     if (err) {
       console.log('数据库连接失败')
-    }else {
+    }else { 
       console.log('成功连接数据库,正在初始化数据库')
       sql.query('CREATE TABLE person(userId smallint(6) unsigned NOT NULL auto_increment,user varchar(255),password varchar(255),name varchar(255),visitNum int(10),PRIMARY KEY (`userId`)) ENGINE=InnoDB DEFAULT CHARSET=gbk', function (err) {
         !err ? console.log('person表创建成功') : console.log(err)
--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.use(express.static(path.join(__dirname, './')))
 app.get('',function(req,res){
 	res.sendFile(__dirname+'/index.html');
 })
-app.listen(8080, () => {
-  console.log(`App listening at port 8080`)
+app.listen(8081, () => {
+  console.log(`App listening at port 8081`)
 })
+
+
